3e436c9703af00be75e399dfd99ba55f9543223f
Nathan Hill <pjsstp@gmail.com>
2019-11-11 16:07:44 -0500

    Updates

---
+++ precache-manifest.1333c3b38aac07c904a36f9d95710b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd0f41f62a4d58ed955f5dbb49ef722",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "2cadd9446cb924f1bbd6",
+    "url": "/new-portfolio/static/css/main.7016e464.chunk.css"
+  },
+  {
+    "revision": "b64b5432e0ef9f2b3e32",
+    "url": "/new-portfolio/static/js/2.a2ba3f49.chunk.js"
+  },
+  {
+    "revision": "2cadd9446cb924f1bbd6",
+    "url": "/new-portfolio/static/js/main.67bae8fb.chunk.js"
+  },
+  {
+    "revision": "4e4c7fa7341efb9c5620",
+    "url": "/new-portfolio/static/js/runtime-main.36659ab8.js"
+  },
+  {
+    "revision": "7f764fa032612b8b9f708feaa25f1f0f",
+    "url": "/new-portfolio/static/media/coffee-cup-desk-257894.7f764fa0.jpg"
+  }
+]);
